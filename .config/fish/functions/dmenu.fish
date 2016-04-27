5548cd705a4488a46fc73660d08967c754ee8abb
Ben Peggs <b.peggs@googlemail.com>
2016-04-27 13:56:59 +0100

    dmenu, rk-app-menu: restyle

--- .config/fish/functions/dmenu.fish
+++ .config/fish/functions/dmenu.fish
@@ -1,3 +1,6 @@
 function dmenu
-  command dmenu -i -fn "OfficeCodePro-11:weight=bold" $argv
+  command dmenu -i -fn "Signika-12:weight=semibold" \
+    -nb "#2a373f" -nf "#babec1" \
+    -sb "#207b88" -sf "#fafbfc" \
+    $argv
 end
