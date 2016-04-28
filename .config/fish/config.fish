00d57e1744cfc8260351d066ac76010adff95106
Ben Peggs <b.peggs@googlemail.com>
2016-04-28 01:00:47 +0100

    fish, termy: better rk-fish-start trick

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,10 @@ end
 
 # rk_fish_start hacks-in a kind of interactive -c option
 if not status -l
-  eval $rk_fish_start
+  set runny $rk_fish_start
+  set -u rk_fish_start
+  if [ -n "$runny" ]
+    fish -c "exec $runny"
+  end
 end
 
