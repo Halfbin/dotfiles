027b9ba7cc74fe6c8f815b7183acaf4c0a580450
Ben Peggs <b.peggs@googlemail.com>
2016-04-30 03:59:03 +0100

    dmenu: font size

--- .config/fish/functions/dmenu.fish
+++ .config/fish/functions/dmenu.fish
@@ -1,5 +1,5 @@
 function dmenu
-  command dmenu -i -fn "Signika-12:weight=semibold" \
+  command dmenu -i -fn "Signika-13:weight=semibold" \
     -nb "#2a373f" -nf "#babec1" \
     -sb "#207b88" -sf "#fafbfc" \
     $argv
