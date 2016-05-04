df5aced8b7c3a9e26ecc1847e2c54b95c5490039
Ben Peggs <b.peggs@googlemail.com>
2016-05-05 00:22:02 +0100

    dmenu: better colour

--- .config/fish/functions/dmenu.fish
+++ .config/fish/functions/dmenu.fish
@@ -1,6 +1,6 @@
 function dmenu
   command dmenu -i -fn "Signika-13:weight=semibold" \
-    -nb "#2a373f" -nf "#babec1" \
+    -nb "#2a373f" -nf "#fafbfc" \
     -sb "#207b88" -sf "#fafbfc" \
     $argv
 end
